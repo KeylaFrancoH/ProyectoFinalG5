81805f36f1a26a34b2de7a2762bda7a54039c853
Ricardo Zaruma <Rjzaruma>
2023-01-22 17:52:59 -0500

    Graficos v1 Zaruma

--- Zaruma.rb
+++ Zaruma.rb
@@ -2,10 +2,27 @@ require 'open-uri' # consultar a la plataforma
 require 'nokogiri' # formatear, parsear a html
 require 'csv' # escribir y leer csv
 
+# nom, id[flag], paises[flag + 1], fortuna[flag + 1], juegos[flag + 1]
+class Jugador
+  attr_accessor :nombre, :id, :pais, :fortuna, :juego
+  def initialize(nombre, id, pais, fortuna, juego)
+    @nombre = nombre
+    @id = id
+    @pais = pais
+    @fortuna = fortuna
+    @juego = juego
+  end
+
+  def guardar
+    CSV.open('informacion.csv', 'a') do |csv|
+      csv << [@nombre, @id, @pais, @fortuna, @juego]
+    end
+  end
+end
 link = 'https://www.esportsearnings.com/players'
 datosHTML = URI.open(link)
 datosParseados = Nokogiri::HTML(datosHTML.read)
-# separamos la pagina
+separamos la pagina
 listaJuegos = datosParseados.css('.detail_list_box')
 cont = 0
 id = []
@@ -26,17 +43,64 @@ listaJuegos.css('.format_row').each do |post|
   fortuna.append(total)
   juegos.append(juego)
 end
-
-CSV.open('archivos/informacion.csv', 'w') do |csv|
-    csv << %w"nom id paises fortuna juegos"
-  end
 flag = 0
 nombres.each do |nom|
-  puts flag
-  puts nom, id[flag], paises[flag + 1], fortuna[flag + 1], juegos[flag + 1]
-  puts
-  CSV.open('archivos/informacion.csv', 'a') do |csv|
-    csv << [nom, id[flag], paises[flag + 1], fortuna[flag + 1], juegos[flag + 1]]
-  end
+  jugador = Jugador.new(nom, id[flag], paises[flag + 1], fortuna[flag + 1], juegos[flag + 1])
+  jugador.guardar
   flag += 1
+end
+des = 0
+CSV.foreach('informacion.csv') do |row|
+  puts des + 1
+  puts row[0]
+  puts row[1]
+  puts row[2]
+  puts row[3]
+  puts row[4]
+  CSV.open('topGanancias.csv', 'a') do |csv|
+    csv << [row[0], row[1], row[2], row[3], row[4]]
+  end
+  des += 1
+  break if des == 10
+end
+juegos = []
+ganancias = []
+CSV.foreach('informacion.csv') do |row|
+  valor = row[3].split('$')
+  ganancia = valor[1].delete(',').to_f.round(1)
+  juego = row[4]
+  if juegos.include? juego
+    ind = juegos.index(juego)
+    ganancias[ind] = ganancias[ind].round(1) + ganancia.round(1)
+  else
+    juegos.append(juego)
+    ganancias.append(ganancia.round(1))
+  end
+end
+
+juegos.each do |elem|
+  ind = juegos.index(elem)
+  ganancia = ganancias[ind]
+  CSV.open('topGananciasxJuegos.csv', 'a') do |csv|
+    csv << [elem, ganancia]
+  end
+end
+paises = []
+cant = []
+CSV.foreach('informacion.csv') do |row|
+  pais = row[2]
+  if paises.include? pais
+    ind = paises.index(pais)
+    cant[ind] += 1
+  else
+    paises.append(pais)
+    cant.append(1)
+  end
+end
+paises.each do |elem|
+  ind = paises.index(elem)
+  cantidad = cant[ind]
+  CSV.open('toppaises.csv', 'a') do |csv|
+    csv << [elem, cantidad]
+  end
 end
---
+++ ZarumaFront.py
@@ -0,0 +1,102 @@
+import  tkinter as tk
+import matplotlib.pyplot
+matplotlib.use('TkAgg')
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg,
+    NavigationToolbar2Tk
+)
+
+def datosGananciasxJugador(nomArchivo):
+    jugador = []
+    ganancia = []
+    fichero = open(nomArchivo)
+    for linea in fichero.readlines():
+        data = linea.split(",")
+        jugador.append(data[0])
+        valor="".join(data[3:-1])
+        cantidad=valor.replace("$", "")
+        ganancia.append(float(cantidad.replace('"', "")))
+    return jugador, ganancia
+
+def datosGananciasxJuegos(nomArchivo):
+    juegos = []
+    ganancia = []
+    fichero = open(nomArchivo)
+    for linea in fichero.readlines():
+        data = linea.split(",")
+        juegos.append(data[0])
+        valor = "".join(data[1:])
+        cantidad = valor.replace("$", "")
+        ganancia.append(float(cantidad.replace('"', "")))
+    return juegos, ganancia
+
+def topPaises(nomArchivo):
+    paises = []
+    cantidades = []
+    fichero = open(nomArchivo)
+    for linea in fichero.readlines():
+        data = linea.split(",")
+        paises.append(data[0])
+        valor = data[1]
+        cantidades.append(valor.replace("\n", ""))
+    return paises[:5], cantidades[:5]
+
+print(datosGananciasxJugador("topGanancias.csv"))
+print(datosGananciasxJuegos("topGananciasxJuegos.csv"))
+print(topPaises("toppaises.csv"))
+
+class App(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.title('PREMIOS OTORGADOS EN VIDEOJUEGOS')
+
+        # prepare data
+        jugador, ganancia = datosGananciasxJugador("topGanancias.csv")
+        juegos, recaudacion = datosGananciasxJuegos("topGananciasxJuegos.csv")
+        paises, cantidad = topPaises("toppaises.csv")
+        # create a figure
+        figure = Figure(figsize=(10, 4), dpi=100)
+        figure2 = Figure(figsize=(10, 4), dpi=100)
+        figure3 = Figure(figsize=(10, 4), dpi=100)
+        # create FigureCanvasTkAgg object
+        figure_canvas = FigureCanvasTkAgg(figure, self)
+        figure_canvas2 = FigureCanvasTkAgg(figure2, self)
+        figure_canvas3 = FigureCanvasTkAgg(figure3, self)
+
+        # create the toolbar
+        NavigationToolbar2Tk(figure_canvas, self)
+        NavigationToolbar2Tk(figure_canvas2, self)
+        NavigationToolbar2Tk(figure_canvas3, self)
+        # create axes
+        axes = figure.add_subplot()
+        axes2 = figure2.add_subplot()
+        axes3 = figure3.add_subplot()
+        # create the barchart
+        #Grafica1
+        axes.barh(jugador, ganancia)
+        axes.set_title('Top 10 Jugadores con más ganancia')
+        axes.set_xlabel('Ganancias')
+        axes.set_ylabel('Jugadores')
+        figure_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1000)
+        #Grafica2
+        axes2.barh(juegos, recaudacion)
+        axes2.set_title('Top 10 Juegos con más dinero repartido')
+        axes2.set_xlabel('Recaudación')
+        axes2.set_ylabel('Juegos')
+        figure_canvas2.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1000)
+        # Grafica3
+        axes3.barh(paises, cantidad)
+        axes3.set_title('Paises con mas Jugadores en el top 100')
+        axes3.set_xlabel('Cantidad')
+        axes3.set_ylabel('Paises')
+        figure_canvas3.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1000)
+
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
